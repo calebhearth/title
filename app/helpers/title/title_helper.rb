2966b502a73b9dc40b6b4e927af41566056856cd
Caleb Thompson <caleb@calebthompson.io>
2014-02-13 14:12:37 -0500

    Find :default key in lookup path to use as default
    If a key isn't found, before using application title or guessing the
    title, look for `:default` in the lookup path.
    
    For example, in `users#show`, `titles.users.default` would be used if
    `titles.users.show` were not present.

--- app/helpers/title/title_helper.rb
+++ app/helpers/title/title_helper.rb
@@ -14,7 +14,7 @@ module Title
       def to_s
         I18n.t(
           [:titles, controller_name, action_name].join('.'),
-          context.merge(default: [application_title, guess_title])
+          context.merge(default: defaults)
         )
       end
 
@@ -30,8 +30,22 @@ module Title
         Rails.application.class.to_s.split('::').first
       end
 
+      def defaults
+        default_keys_in_lookup_path + [application_title, guess_title]
+      end
+
       def controller_name
         controller_path.gsub('/', '.')
+      end
+
+      def default_keys_in_lookup_path
+        defaults = []
+        lookup_path = controller_name.split('.')
+        while lookup_path.length > 0
+          defaults << ['titles', *lookup_path, 'default'].join('.').to_sym
+          lookup_path.pop
+        end
+        defaults.reverse
       end
     end
   end
--- title.gemspec
+++ title.gemspec
@@ -22,6 +22,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'rake'
   spec.add_development_dependency 'rspec'
   spec.add_development_dependency 'coveralls'
+  spec.add_development_dependency 'pry'
 
   spec.add_dependency 'rails', '>= 3.1'
   spec.add_dependency 'i18n'
