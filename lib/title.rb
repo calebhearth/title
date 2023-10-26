8678878814cbf55117dd47ed73d626c5d37448dc
Caleb Hearth <caleb@calebhearth.com>
2023-10-26 10:33:01 -0600

    Fix Rails 7.1.0 enforcing active_support dep
    Similar to https://github.com/rails/rails/issues/49495#issuecomment-1749085658

--- lib/title.rb
+++ lib/title.rb
@@ -2,6 +2,7 @@ app = File.expand_path('../../app', __FILE__)
 $LOAD_PATH.unshift(app) unless $LOAD_PATH.include?(app)
 
 require 'title/version'
+require 'active_support'
 require 'rails/engine'
 require 'title/engine'
 require 'helpers/title/title_helper'
